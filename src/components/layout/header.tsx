d2e33d345e60e0e7a21c5fff2a2c49278b2a8ba2
Olusegun Ife tomiwa <olusegunifetomiwa2000@gmail.com>
2025-11-21 05:15:37 +0000

    Remove the case study page for now

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,6 @@ const navLinks = [
   { href: '/', label: 'Home' },
   { href: '/about', label: 'About' },
   { href: '/services', label: 'Services' },
-  { href: '/case-studies', label: 'Case Studies' },
   { href: '/work', label: 'Work' },
   { href: '/contact', label: 'Contact' },
 ];
